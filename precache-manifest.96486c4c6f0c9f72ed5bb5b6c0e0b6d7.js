043736f9d1f3dd1f671ccaf139ff42d575fe7f1d
KaishAli <360mohdkaishali.cs@gmail.com>
2022-03-02 02:36:36 +0530

    Updates

---
+++ precache-manifest.96486c4c6f0c9f72ed5bb5b6c0e0b6d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "586103ae1edf593a0593",
+    "url": "/React-Digital-Compass-Clock/static/css/main.3860e501.chunk.css"
+  },
+  {
+    "revision": "586103ae1edf593a0593",
+    "url": "/React-Digital-Compass-Clock/static/js/main.586103ae.chunk.js"
+  },
+  {
+    "revision": "6d60b09eca5d924a41e7",
+    "url": "/React-Digital-Compass-Clock/static/js/1.6d60b09e.chunk.js"
+  },
+  {
+    "revision": "42515df623dcecd10e4d",
+    "url": "/React-Digital-Compass-Clock/static/js/runtime~main.42515df6.js"
+  },
+  {
+    "revision": "35dcbf350b8825191b2812b4c18f13a8",
+    "url": "/React-Digital-Compass-Clock/index.html"
+  }
+];
